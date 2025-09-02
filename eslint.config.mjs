fe4db28158bdbee721dfcc070ed26e9c6e347393
Armando Ribamar <armando.ribamar@gmail.com>
2025-09-02 13:54:05 -0300

    fix: improve error handling in bootstrap function and update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,12 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked, // ou .recommended
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
@@ -17,18 +17,18 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-function-return-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
-);
+];
