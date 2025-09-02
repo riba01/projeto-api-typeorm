11b5d25168dec120e04053a3f58f099c0bab7aaa
Armando Ribamar <armando.ribamar@gmail.com>
2025-09-02 16:27:24 -0300

    feat: update greeting message and add POST endpoint in AppController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World, Jesus is Real!"', () => {
+      expect(appController.getHello()).toBe('Hello World, Jesus is Real!');
     });
   });
 });
